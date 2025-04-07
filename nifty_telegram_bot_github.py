9926bc8cc9b9ec943181aa5a08f9d21734e7dd12
nitinkumarjain009 <kanakjain2309@gmail.com>
2025-04-08 03:50:31 +0530

    nifty_telegram_bot_github.py

--- nifty_telegram_bot_github.py
+++ nifty_telegram_bot_github.py
@@ -4,7 +4,7 @@ import os
 import logging
 import datetime
 
-# Set up logging
+# Set up logging1
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
