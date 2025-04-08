e8002ab209fcb08c955c35180ca438ccb213db35
nitinkumarjain009 <kanakjain2309@gmail.com>
2025-04-09 01:20:12 +0530

    Nifty_RSIC_Analysis.py

--- Nifty_RSIC_Analysis.py
+++ Nifty_RSIC_Analysis.py
@@ -615,9 +615,9 @@ def create_templates():
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
-<script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <script>
     // Initialize DataTable
     let allStocksTable;
     
@@ -833,4 +833,7 @@ def create_templates():
             fetchData();
         });
     });
-</script>
+    </script>
+</body>
+</html>
+""")
